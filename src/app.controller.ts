e0450fa2f08bc3f4d0a9d58de78b3f68ef62d4ed
yeongwoo <jyy0223@naver.com>
2024-01-17 01:12:26 +0900

    [add] column 기능 확인을 위한 get, post, patch API 구현

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Param, Patch, Post } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserModel } from './entity/user.entity';
+import { Repository } from 'typeorm';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    @InjectRepository(UserModel)
+    private readonly userModelRepository: Repository<UserModel>,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('users')
+  async postUsers() {
+    return this.userModelRepository.save({
+      title: 'title',
+    });
+  }
+
+  @Get('users')
+  async getUsers() {
+    return await this.userModelRepository.find();
+  }
+
+  @Patch('users/:id')
+  async patchUsers(@Param('id') id: string) {
+    const user = await this.userModelRepository.findOne({
+      where: {
+        id: id,
+      },
+    })
+    return await this.userModelRepository.save({
+      ...user,
+      title: user.title + '0',
+    })
   }
 }
