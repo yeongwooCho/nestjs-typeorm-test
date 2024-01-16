4f4aa50df63dbd9bb31448bbafede16ad9db07b9
yeongwoo <jyy0223@naver.com>
2024-01-17 00:06:18 +0900

    [add] typeorm settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { myDatabaseInfo } from '../my_settings';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: myDatabaseInfo.host,
+      port: myDatabaseInfo.port,
+      username: myDatabaseInfo.username,
+      password: myDatabaseInfo.password,
+      database: myDatabaseInfo.database,
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
