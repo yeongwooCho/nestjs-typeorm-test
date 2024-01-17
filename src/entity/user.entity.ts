af25e83d52c6604ace5773e1ea7fd37ae86b6e8a
yeongwoo <jyy0223@naver.com>
2024-01-17 17:31:50 +0900

    [add] UserModel entity 정의 + enum Column

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -5,18 +5,49 @@ import {
   Generated,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
-  VersionColumn,
-} from 'typeorm';
+  VersionColumn
+} from "typeorm";
+
+export enum Role {
+  USER = "user",
+  ADMIN = "admin",
+}
 
 @Entity()
 export class UserModel {
   // @PrimaryGeneratedColumn() // 자동으로 +1 아이디 생성
   // @PrimaryColumn() // 직접 생성할 primary key
-  @PrimaryGeneratedColumn('uuid') // uuid 자동 생성
+  @PrimaryGeneratedColumn("uuid") // uuid 자동 생성
   id: string;
 
-  @Column()
+  @Column({
+    // DB에서 인지하는 칼럼 타입(자동 완성)
+    // default: property type
+    type: "varchar",
+    // DB 칼럼 이름
+    // default: property name
+    name: "_title",
+    // 값의 길이: 입력 가능한 글자 길이
+    length: 300,
+    // null 가능?
+    nullable: true,
+    // 업데이트 가능 여부
+    update: true,
+    // find() 를 실핼할때 기본으로 값을 불러올지?
+    select: true, // default: true
+    // 아무것도 입력이 안 됐을때 기본 값
+    default: "왜들그리다운돼있어~",
+    // 칼럼 중 유일무이한 값이 되어야 하는가
+    unique: false
+  })
   title: string;
+
+  @Column({
+    type: 'enum',
+    enum: Role,
+    default: Role.USER,
+  })
+  role: Role;
 
   // 데이터가 생성되는 날짜시간으로 자동 지정
   @CreateDateColumn()
@@ -33,6 +64,6 @@ export class UserModel {
   version: number;
 
   @Column()
-  @Generated('uuid')
+  @Generated("uuid")
   additionalId: string;
 }
