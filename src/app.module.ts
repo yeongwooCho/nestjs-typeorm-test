49461aa5c0d2e02ada1ec59d9d74867168697972
yeongwoo <jyy0223@naver.com>
2024-01-17 21:04:47 +0900

    [add] Single Table Inheritance: 상속으로 관리하지만 실제 테이블은 하나의 테이블로 모든 entity가 관리

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { myDatabaseInfo } from '../my_settings';
 import { UserModel } from './entity/user.entity';
 import { StudentModel, TeacherModel } from "./entity/person.entity";
-import { BookModel, CarModel } from "./entity/inheritance.entity";
+import { AirplaneModel, BookModel, CarModel, ComputerModel, SingleBaseModel } from "./entity/inheritance.entity";
 
 @Module({
   imports: [
@@ -19,7 +19,16 @@ import { BookModel, CarModel } from "./entity/inheritance.entity";
       username: myDatabaseInfo.username,
       password: myDatabaseInfo.password,
       database: myDatabaseInfo.database,
-      entities: [UserModel, StudentModel, TeacherModel, BookModel, CarModel],
+      entities: [
+        UserModel,
+        StudentModel,
+        TeacherModel,
+        BookModel,
+        CarModel,
+        SingleBaseModel,
+        ComputerModel,
+        AirplaneModel,
+      ],
       synchronize: true,
     }),
   ],
--- src/entity/inheritance.entity.ts
+++ src/entity/inheritance.entity.ts
@@ -1,4 +1,12 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import {
+  ChildEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  TableInheritance,
+  UpdateDateColumn
+} from "typeorm";
 
 export class BaseModel {
   @PrimaryGeneratedColumn()
@@ -21,4 +29,35 @@ export class BookModel extends BaseModel {
 export class CarModel extends BaseModel {
   @Column()
   brand: string;
+}
+
+
+@Entity()
+@TableInheritance({
+  column: {
+    name: 'type',
+    type: 'varchar',
+  }
+})
+export class SingleBaseModel {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
+
+@ChildEntity()
+export class ComputerModel extends SingleBaseModel {
+  @Column()
+  brand: string;
+}
+
+@ChildEntity()
+export class AirplaneModel extends SingleBaseModel {
+  @Column()
+  country: string;
 }
