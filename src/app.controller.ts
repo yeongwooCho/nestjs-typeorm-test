a5a49c9b10b2ea4ae48a39d001ea246365c2484f
yeongwoo <jyy0223@naver.com>
2024-01-18 19:46:58 +0900

    [add] where 조건에서 사용되는 utility 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,18 @@
 import { Controller, Delete, Get, Param, Patch, Post } from "@nestjs/common";
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserModel } from './entity/user.entity';
-import { Repository } from 'typeorm';
+import {
+  Between,
+  Equal,
+  ILike, In, IsNull,
+  LessThan,
+  LessThanOrEqual,
+  Like,
+  MoreThan,
+  MoreThanOrEqual,
+  Not,
+  Repository
+} from "typeorm";
 import { ProfileModel } from "./entity/profile.entity";
 import { PostModel } from "./entity/post.entity";
 import { TagModel } from "./entity/tag.entity";
@@ -31,7 +42,30 @@ export class AppController {
   @Get('users')
   async getUsers() {
     return await this.userRepository.find({
-      where: {},
+      where: {
+        // // null 인가?
+        // id: IsNull(),
+        // // 이 안에 있는가?
+        // id: In([1,3,48,99]),
+        // // 사이 값
+        // id: Between(10,15),
+        // // 대소문자 구분 안하는 유사값
+        // email: ILike('%GOOGLE%'),
+        // // 유사값
+        // email: Like('%0%'),
+        // // 같은 경우
+        // id: Equal(30),
+        // // 크거나 같은 경우 가져오기
+        // id: MoreThanOrEqual(30),
+        // // 큰 경우 가져오기
+        // id: MoreThan(30),
+        // // 작거나 같은 경우 가져오기
+        // id: LessThanOrEqual(30),
+        // // 적은 경우 가져오기
+        // id: LessThan(30),
+        // // 아닌 경우 가져오기
+        // id: Not(1),
+      },
     });
   }
 
